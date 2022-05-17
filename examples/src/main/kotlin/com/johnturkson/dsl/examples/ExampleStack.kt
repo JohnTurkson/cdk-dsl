c3f77a97bbd14e1f8299d41f2bb1ac6c9f186c85
John Turkson <git@johnturkson.com>
2022-05-17 15:57:34 -0700

    Add function example

--- examples/src/main/kotlin/com/johnturkson/dsl/examples/ExampleStack.kt
+++ examples/src/main/kotlin/com/johnturkson/dsl/examples/ExampleStack.kt
@@ -2,7 +2,9 @@ package com.johnturkson.dsl.examples
 
 import software.amazon.awscdk.Stack
 import software.amazon.awscdk.StackProps
+import software.amazon.awscdk.services.lambda.Code
 import software.amazon.awscdk.services.lambda.Function
+import software.amazon.awscdk.services.lambda.Runtime
 import software.amazon.awscdk.services.s3.Bucket
 import software.constructs.Construct
 
@@ -12,7 +14,12 @@ class ExampleStack(
     props: StackProps? = null,
 ) : Stack(parent, name, props) {
     init {
-        Function.Builder.create(this, "")
+        val code = """function main() { console.log("Hello World") }"""
+        Function.Builder.create(this, "ExampleFunction")
+            .runtime(Runtime.NODEJS_16_X)
+            .handler("index.main")
+            .code(Code.fromInline(code))
+            .build()
         Bucket.Builder.create(this, "ExampleBucket").build()
     }
 }
